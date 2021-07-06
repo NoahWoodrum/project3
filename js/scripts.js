5cfc8bb0c3c065bccad734dc88cfd9b18d05252d
Noah Woodrum <heymymomthinksimcool@gmail.com>
2021-07-05 17:39:44 -0700

    pass 2nd test

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 function beepboop(number){
   let returnedOutput= [];
+  if (i.toString().includes("1")) {
+    returnedOutput.push("Beep!")
+  } else {
+    returnedOutput.push(i)
+  for (let i = 0; i <= number; i++) {
   if (number === "0") return returnedArray
+  }}
+  return returnedOutput;
 }
