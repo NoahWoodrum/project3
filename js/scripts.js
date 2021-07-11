c23a55b51b7f28b9b4ca5ebcdeeb156a3be2674c
Noah Woodrum <heymymomthinksimcool@gmail.com>
2021-07-10 22:14:10 -0700

    working js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 function beepboop(number){
   let returnedOutput= [];
   for (let i = 0; i <= number; i++) {
-    if (i.toString().includes("1")) {
-    returnedOutput.push("Beep!")
+    if (i.toString().includes("3")) {
+    returnedOutput.push("Won't you be my neighbor!")
     } else if (i.toString().includes("2")){
     returnedOutput.push("Boop!")
+    } else if (i.toString().includes("1")){
+    returnedOutput.push("Beep!")
     } else {
     returnedOutput.push(i)
-  }  
-    if (number === "0") return returnedArray
-    }
-  return returnedOutput;
+  }}  
+  
+  return returnedOutput.join(" ");
 }
